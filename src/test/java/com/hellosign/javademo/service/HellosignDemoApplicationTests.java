f686d9fd41b707ac6595ffcea9543dc1ca225836
rvsathe <reshma30m@gmail.com>
2021-12-11 18:00:43 +0530

    Correcting the test package

--- src/test/java/com/hellosign/javademo/service/HellosignDemoApplicationTests.java
+++ src/test/java/com/hellosign/javademo/service/HellosignDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hamdiBouallegue.hellosigndemo;
+package com.hellosign.javademo.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
