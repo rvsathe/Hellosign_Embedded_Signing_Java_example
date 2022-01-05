b144f98a65aa67af706880b8de51450be95ff2da
rvsathe <reshma30m@gmail.com>
2022-01-05 18:02:48 +0530

    Removing sensitive information

--- src/main/java/com/hellosign/javademo/service/HelloSignService.java
+++ src/main/java/com/hellosign/javademo/service/HelloSignService.java
@@ -15,23 +15,22 @@ public class HelloSignService {
 	public String sendEmbeddedSignatureRequest() throws HelloSignException {
 
 		TemplateSignatureRequest request = new TemplateSignatureRequest();
-		request.setTemplateId("10a9cf98b0e0eba0ce276a9ab026abb19d59e6ab");
+		request.setTemplateId("<TEMPLATE_ID_FOR_DOCUMENT>");
 		request.setSubject("Please review and Sign");
 		request.setMessage("Please review and Sign");
-		request.setSigner("Client", "testmailbox2021@gmail.com","Test User");
+		request.setSigner("Client", "<RECIPIENT_EMAIL_ADDRESS>","<RECIPIENT_NAME>");
 		request.setTestMode(true); // Only if account is not an Enterprise account.
 
-		String clientId = "5e36f64b9f82a48cb6db34fb3483f7dc";
+		String clientId = "<CLIENT_ID_OF_YOUR_APP>";
 		EmbeddedRequest embedReq = new EmbeddedRequest(clientId, request);
 		//Create a HelloSign Client
 		HelloSignClient helloSignclient = new HelloSignClient(
-				"a2faee0f49b83377213dfe2cb47b3eee84c40ad4cf7cbf9e43ca81b9876d0941");
-		//helloSignclient.setCallback("https://4252-115-96-171-211.ngrok.io/api/hellosign/webhook");
+				"<YOUR_API_TOKEN_KEY>");
 		//create new Embedded Signature Request
 		SignatureRequest newRequest = (SignatureRequest) helloSignclient.createEmbeddedRequest(embedReq);
 
-		Signature signature = newRequest.getSignature("testmailbox2021@gmail.com","Test User");
-		String signatureId =  signature.getId(); //"<SIGNATURE_ID_YOU_GET_FROM_EMBEDDED_SIGNING_REQ_RESPONSE>";
+		Signature signature = newRequest.getSignature("<RECIPIENT_EMAIL_ADDRESS>","<RECIPIENT_NAME>");
+		String signatureId =  signature.getId();
 		EmbeddedResponse response = helloSignclient.getEmbeddedSignUrl(signatureId);
 		String url = response.getSignUrl();
 
